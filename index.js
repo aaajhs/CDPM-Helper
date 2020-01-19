010a5c03bed1901b786610b4a46c241ea40e7dc3
Danny Jang <aaajhs@gmail.com>
2020-01-20 01:32:08 +0900

    Update

--- index.js
+++ index.js
@@ -40,31 +40,23 @@ function alertMaintenance (mStart){
   //   }
   // }
 
-  while(mStart.getTime() - Date.now() > 90000){
-    if(mStart.getTime() - Date.now() == 120000){
-      slack.chat.postMessage({
-        token: 'xoxp-734466708384-734473058917-873557841859-3dd4345d6fb7271677b9cda17cd3541e',
-        channel: 'testing-slack-bots',
-        text: '서버 점검 2분 전'});
-    }
+  if(mStart.getTime() - Date.now() == 120000){
+    slack.chat.postMessage({
+      token: 'xoxp-734466708384-734473058917-873557841859-3dd4345d6fb7271677b9cda17cd3541e',
+      channel: 'testing-slack-bots',
+      text: '서버 점검 2분 전'});
   }
-
-  while(mStart.getTime() - Date.now() > 30000){
-    if(mStart.getTime() - Date.now() == 60000){
-      slack.chat.postMessage({
-        token: 'xoxp-734466708384-734473058917-873557841859-3dd4345d6fb7271677b9cda17cd3541e',
-        channel: 'testing-slack-bots',
-        text: '서버 점검 1분 전'});
-    }
+  else if(mStart.getTime() - Date.now() == 60000){
+    slack.chat.postMessage({
+      token: 'xoxp-734466708384-734473058917-873557841859-3dd4345d6fb7271677b9cda17cd3541e',
+      channel: 'testing-slack-bots',
+      text: '서버 점검 1분 전'});
   }
-
-  while(mStart.getTime() - Date.now() < 30000){
-    if(mStart.getTime() - Date.now() == 0){
-      slack.chat.postMessage({
-        token: 'xoxp-734466708384-734473058917-873557841859-3dd4345d6fb7271677b9cda17cd3541e',
-        channel: 'testing-slack-bots',
-        text: '서버 점검 시작'});
-    }
+  else if(mStart.getTime() - Date.now() == 0){
+    slack.chat.postMessage({
+      token: 'xoxp-734466708384-734473058917-873557841859-3dd4345d6fb7271677b9cda17cd3541e',
+      channel: 'testing-slack-bots',
+      text: '서버 점검 시작'});
   }
 
 }
@@ -74,7 +66,9 @@ app.post("/setmstart", function(req, res) {
   var mStartTime = new Date(req.body.text);  //format: 2011-10-10T14:48:00
   console.log(mStartTime);
   res.send(200);
-  alertMaintenance(mStartTime);
+  while(Date.now() <= mStartTime.getTime()){
+    alertMaintenance(mStartTime);
+  }
 });
 
 
