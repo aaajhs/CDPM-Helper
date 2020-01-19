0817379abb4d899742a6ea6fdbcd8b60b5091258
Danny Jang <aaajhs@gmail.com>
2020-01-20 01:01:01 +0900

    Update

--- index.js
+++ index.js
@@ -11,32 +11,53 @@ app.use(bodyParser.urlencoded({
 }));
 
 function alertMaintenance (mStart){
+  // while(Date.now() <= mStart.getTime()){
+  //   //console.log(mStart.getTime() - Date.now());
+  //   switch (mStart.getTime() - Date.now()) {
+  //     case (120000):
+  //       //console.log("30 minutes before");
+  //       slack.chat.postMessage({
+  //         token: 'xoxp-734466708384-734473058917-873557841859-3dd4345d6fb7271677b9cda17cd3541e',
+  //         channel: 'testing-slack-bots',
+  //         text: '서버 점검 2분 전'});
+  //       break;
+  //     case (60000):
+  //       //console.log(mStartTime.getTime() - Date.now());
+  //       slack.chat.postMessage({
+  //         token: 'xoxp-734466708384-734473058917-873557841859-3dd4345d6fb7271677b9cda17cd3541e',
+  //         channel: 'testing-slack-bots',
+  //         text: '서버 점검 1분 전'});
+  //       break;
+  //     case (0):
+  //       slack.chat.postMessage({
+  //         token: 'xoxp-734466708384-734473058917-873557841859-3dd4345d6fb7271677b9cda17cd3541e',
+  //         channel: 'testing-slack-bots',
+  //         text: '서버 점검 시작'});
+  //         console.log(mStart.getTime() - Date.now());
+  //         break;
+  //     default:
+  //       break;
+  //   }
+  // }
+
   while(Date.now() <= mStart.getTime()){
-    //console.log(mStart.getTime() - Date.now());
-    switch (mStart.getTime() - Date.now()) {
-      case (120000):
-        //console.log("30 minutes before");
-        slack.chat.postMessage({
-          token: 'xoxp-734466708384-734473058917-873557841859-3dd4345d6fb7271677b9cda17cd3541e',
-          channel: 'testing-slack-bots',
-          text: '서버 점검 2분 전'});
-        break;
-      case (60000):
-        //console.log(mStartTime.getTime() - Date.now());
-        slack.chat.postMessage({
-          token: 'xoxp-734466708384-734473058917-873557841859-3dd4345d6fb7271677b9cda17cd3541e',
-          channel: 'testing-slack-bots',
-          text: '서버 점검 1분 전'});
-        break;
-      case (0):
-        slack.chat.postMessage({
-          token: 'xoxp-734466708384-734473058917-873557841859-3dd4345d6fb7271677b9cda17cd3541e',
-          channel: 'testing-slack-bots',
-          text: '서버 점검 시작'});
-          console.log(mStart.getTime() - Date.now());
-          break;
-      default:
-        break;
+    if(mStart.getTime() - Date.now() == 120000){
+      slack.chat.postMessage({
+        token: 'xoxp-734466708384-734473058917-873557841859-3dd4345d6fb7271677b9cda17cd3541e',
+        channel: 'testing-slack-bots',
+        text: '서버 점검 2분 전'});
+    }
+    else if(mStart.getTime() - Date.now() == 60000){
+      slack.chat.postMessage({
+        token: 'xoxp-734466708384-734473058917-873557841859-3dd4345d6fb7271677b9cda17cd3541e',
+        channel: 'testing-slack-bots',
+        text: '서버 점검 1분 전'});
+    }
+    else if(mStart.getTime() - Date.now() == 0){
+      slack.chat.postMessage({
+        token: 'xoxp-734466708384-734473058917-873557841859-3dd4345d6fb7271677b9cda17cd3541e',
+        channel: 'testing-slack-bots',
+        text: '서버 점검 시작'});
     }
   }
 }
