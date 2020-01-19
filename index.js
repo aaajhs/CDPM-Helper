c0c6c7fdada75ca0640863f40bc8977ca3291acf
Danny Jang <aaajhs@gmail.com>
2020-01-20 00:23:08 +0900

    Update

--- index.js
+++ index.js
@@ -87,19 +87,19 @@ function alertMaintenance (mStart){
 
   while(Date.now() <= mStartTime.getTime()){
     switch (timeDifference) {
-      case (600000):
+      case (60000):
         //console.log(mStartTime.getTime() - Date.now());
         slack.chat.postMessage({
           token: 'xoxp-734466708384-734473058917-873557841859-3dd4345d6fb7271677b9cda17cd3541e',
           channel: 'testing-slack-bots',
-          text: '서버 점검 10분 전'});
+          text: '서버 점검 1분 전'});
         break;
-      case (1800000):
+      case (180000):
         //console.log("30 minutes before");
         slack.chat.postMessage({
           token: 'xoxp-734466708384-734473058917-873557841859-3dd4345d6fb7271677b9cda17cd3541e',
           channel: 'testing-slack-bots',
-          text: '서버 점검 30분 전'});
+          text: '서버 점검 3분 전'});
         break;
       case (0):
       slack.chat.postMessage({
@@ -117,9 +117,10 @@ function alertMaintenance (mStart){
 //Command Handler
 app.post("/setmstart", function(req, res) {
   var mStartTime = new Date(req.body.text);  //format: 2011-10-10T14:48:00
+
+    res.send(200);
   alertMaintenance(mStartTime);
 
-  res.send(200);
 });
 
 
