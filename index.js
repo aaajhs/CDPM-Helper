a84184b75bda348729dc361f07af05caafe8788e
Danny Jang <teru@Dannys-MacBook-Pro-2.local>
2020-01-22 17:32:36 +0900

    Update

--- index.js
+++ index.js
@@ -12,43 +12,48 @@ app.use(bodyParser.urlencoded({
 
 function sendMessageTo(channel, text) {
   slack.chat.postMessage({
-    token: 'xoxp-734466708384-734473058917-873557841859-3dd4345d6fb7271677b9cda17cd3541e',
+    token: process.env.token,
     channel,
     text,
   });
 }
 
-function alertMaintenance (mStart){
-  const delayUntilTwoMinuteBefore = mStart.getTime() - (2 * 60 * 1000) - Date.now();
-  const delayUntilOneMinuteBefore = mStart.getTime() - (1 * 60 * 1000) - Date.now();
-  const delayUntilMaintenance = mStart.getTime() - Date.now();
+function alertMaintenance (targetTime){
+  const mStartMinusTwo = targetTime.getTime() - (2 * 60 * 1000) - Date.now();
+  const mStartMinusOne = targetTime.getTime() - (1 * 60 * 1000) - Date.now();
+  const mStart = targetTime.getTime() - Date.now();
 
   setTimeout(function () {
-    sendMessageTo('testing-slack-bots', '서버 점검 2분 전');
-  }, delayUntilTwoMinuteBefore);
+    sendMessageTo('general', '서버 점검 2분 전');
+  }, mStartMinusTwo);
 
   setTimeout(function () {
-    sendMessageTo('testing-slack-bots', '서버 점검 1분 전');
-  } , delayUntilOneMinuteBefore);
+    sendMessageTo('general', '서버 점검 1분 전');
+  } , mStartMinusOne);
 
   setTimeout(function () {
-    sendMessageTo('testing-slack-bots', '서버 점검 시작');
-  }, delayUntilMaintenance);
+    sendMessageTo('general', '서버 점검 시작');
+  }, mStart);
 
   // setTimeout(() => {
   //   console.log('this function called after 2 sec');
   // }, 2000);
-
 }
 
 //Command Handler
 app.post("/setmstart", function(req, res) {
   var mStartTime = new Date(req.body.text);  //format: 2011-10-10T14:48:00
   console.log(mStartTime);
-  res.send(200);
+  res.send("OK, maintenance start time has been set.");
   alertMaintenance(mStartTime);
 });
 
+app.post("/setmend", (req, res) => {
+  var mEndTime = new Date(req.body.text); //format: 2011-10-10T14:48:00
+  console.log(mEndTime);
+  res.send("OK, maintenance end time has been set.");
+  alertMaintenance(mEndTime);
+});
 
 app.listen(process.env.PORT, function() {
   console.log("Server is running");
