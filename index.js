f60d91d65d0f85f00b853bf77e5bf298364a4089
Danny Jang <teru@Dannys-MacBook-Pro-2.local>
2020-01-23 19:36:24 +0900

    Ready for use in console_production

--- index.js
+++ index.js
@@ -30,28 +30,28 @@ function alertMaintenance (targetTime, startEnd){
 
   setTimeout(function () {
     if(startEnd.localeCompare("start") == 0){
-      sendMessageTo('bot-testspace', '서버 점검 시작 20분 전');
+      sendMessageTo('console_production', '서버 점검 시작 20분 전');
     }
     else if(startEnd.localeCompare("end") == 0){
-      sendMessageTo('bot-testspace', '서버 점검 종료 20분 전');
+      sendMessageTo('console_production', '서버 점검 종료 20분 전');
     }
   }, mTimeMinusTwenty);
 
   setTimeout(function () {
     if(startEnd.localeCompare("start") == 0){
-      sendMessageTo('bot-testspace', '서버 점검 시작 5분 전');
+      sendMessageTo('console_production', '서버 점검 시작 5분 전');
     }
     else if(startEnd.localeCompare("end") == 0){
-      sendMessageTo('bot-testspace', '서버 점검 종료 5분 전')
+      sendMessageTo('console_production', '서버 점검 종료 5분 전')
     }
   } , mTimeMinusFive);
 
   setTimeout(function () {
     if(startEnd.localeCompare("start") == 0){
-      sendMessageTo('bot-testspace', '서버 점검 시작');
+      sendMessageTo('console_production', '서버 점검 시작');
     }
     else if(startEnd.localeCompare("end") == 0){
-      sendMessageTo('bot-testspace', '서버 점검 종료');
+      sendMessageTo('console_production', '서버 점검 종료');
     }
 
   }, mTime);
@@ -65,14 +65,14 @@ function alertMaintenance (targetTime, startEnd){
 app.post("/setmstart", function(req, res) {
   var mStartTime = new Date(req.body.text);  //format: 2011-10-10T14:48:00
   var mTimeType = "start";
-  res.send("서버 점검 시작시간이 설정되었습니다.");
+  res.send("OK, maintenance start time has been set.");
   alertMaintenance(mStartTime, mTimeType);
 });
 
 app.post("/setmend", (req, res) => {
   var mEndTime = new Date(req.body.text); //format: 2011-10-10T14:48:00
   var mTimeType = "end";
-  res.send("서버 점검 종료시간이 설정되었습니다.");
+  res.send("OK, maintenance end time has been set.");
   alertMaintenance(mEndTime, mTimeType);
 });
 
