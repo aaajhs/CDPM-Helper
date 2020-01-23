03eb25d3ede8c26ca37959eff80d0732a59809d3
Danny Jang <teru@Dannys-MacBook-Pro-2.local>
2020-01-23 19:30:32 +0900

    Adjusted notification time

--- index.js
+++ index.js
@@ -24,27 +24,27 @@ function sendMessageTo(channel, text) {
 }
 
 function alertMaintenance (targetTime, startEnd){
-  const mTimeMinusTwo = targetTime.getTime() - (2 * 60 * 1000) - Date.now();
-  const mTimeMinusOne = targetTime.getTime() - (1 * 60 * 1000) - Date.now();
+  const mTimeMinusTwenty = targetTime.getTime() - (20 * 60 * 1000) - Date.now();
+  const mTimeMinusFive = targetTime.getTime() - (5 * 60 * 1000) - Date.now();
   const mTime = targetTime.getTime() - Date.now();
 
   setTimeout(function () {
     if(startEnd.localeCompare("start") == 0){
-      sendMessageTo('bot-testspace', '서버 점검 시작 2분 전');
+      sendMessageTo('bot-testspace', '서버 점검 시작 20분 전');
     }
     else if(startEnd.localeCompare("end") == 0){
-      sendMessageTo('bot-testspace', '서버 점검 종료 2분 전');
+      sendMessageTo('bot-testspace', '서버 점검 종료 20분 전');
     }
-  }, mTimeMinusTwo);
+  }, mTimeMinusTwenty);
 
   setTimeout(function () {
     if(startEnd.localeCompare("start") == 0){
-      sendMessageTo('bot-testspace', '서버 점검 시작 1분 전');
+      sendMessageTo('bot-testspace', '서버 점검 시작 5분 전');
     }
     else if(startEnd.localeCompare("end") == 0){
-      sendMessageTo('bot-testspace', '서버 점검 종료 1분 전')
+      sendMessageTo('bot-testspace', '서버 점검 종료 5분 전')
     }
-  } , mTimeMinusOne);
+  } , mTimeMinusFive);
 
   setTimeout(function () {
     if(startEnd.localeCompare("start") == 0){
@@ -65,14 +65,14 @@ function alertMaintenance (targetTime, startEnd){
 app.post("/setmstart", function(req, res) {
   var mStartTime = new Date(req.body.text);  //format: 2011-10-10T14:48:00
   var mTimeType = "start";
-  res.send("OK, maintenance start time has been set.");
+  res.send("서버 점검 시작시간이 설정되었습니다.");
   alertMaintenance(mStartTime, mTimeType);
 });
 
 app.post("/setmend", (req, res) => {
   var mEndTime = new Date(req.body.text); //format: 2011-10-10T14:48:00
   var mTimeType = "end";
-  res.send("OK, maintenance end time has been set.");
+  res.send("서버 점검 종료시간이 설정되었습니다.");
   alertMaintenance(mEndTime, mTimeType);
 });
 
