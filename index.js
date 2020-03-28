5b9198ccc31d49edf33b46b2da023598268075ec
Danny Jang <teru@Dannys-MacBook-Pro-2.local>
2020-03-29 04:58:31 +0900

    Reenable mRoutine

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ function sendTimedMessage(channel, text, time) {
 
 // function for maintenance reminder routine
 function mRoutine(targetChannel, startTime, endTime, updateDate){ // startTime, endTime is in 2020-03-12T12:00:00 format
-  //mReminder(targetChannel, true, startTime, updateDate);
+  mReminder(targetChannel, true, startTime, updateDate);
   mReminder(targetChannel, false, endTime, updateDate);
 }
 // end
