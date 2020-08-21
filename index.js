6ebfca2ada309071205e11aa1cb60d057c320b4e
Danny Jang <teru@Dannys-MacBook-Pro-2.local>
2020-08-22 00:07:18 +0900

    Return view payload to shortcut

--- index.js
+++ index.js
@@ -231,75 +231,82 @@ app.post("/consoleupdate", (req, res) => {
 
 app.post("/interactive-endpoint", (req, res) => {
   console.log(req.body.payload.callback_id);
+  const payload = req.body.payload;
+  const obj = JSON.parse(payload);
+
+  slack.views.open({
+    token: process.env.token,
+    trigger_id: obj.trigger_id,
+    view: {
+      "title": {
+        "type": "plain_text",
+        "text": "My App",
+        "emoji": true
+      },
+      "submit": {
+        "type": "plain_text",
+        "text": "Submit",
+        "emoji": true
+      },
+      "type": "modal",
+      "close": {
+        "type": "plain_text",
+        "text": "Cancel",
+        "emoji": true
+      },
+      "blocks": [{
+          "type": "section",
+          "text": {
+            "type": "plain_text",
+            "text": "*Contents Update*\n- 점검 시작 30분 전, 10분 전 리마인더\n- 점검 스레드 생성\n- 점검 종료 30분 전, 10분 전 리마인더\n- (Optional) PTS 종료 리마인더\n*Hotfix/PTS*\n- 배포 시작 30분 전, 10분 전, 시작 시점 리마인더",
+            "emoji": true
+          }
+        },
+        {
+          "type": "divider"
+        },
+        {
+          "type": "input",
+          "element": {
+            "type": "static_select",
+            "placeholder": {
+              "type": "plain_text",
+              "text": "Select an item",
+              "emoji": true
+            },
+            "options": [{
+                "text": {
+                  "type": "plain_text",
+                  "text": "Contents Update",
+                  "emoji": true
+                },
+                "value": "cu"
+              },
+              {
+                "text": {
+                  "type": "plain_text",
+                  "text": "Hotfix/PTS",
+                  "emoji": true
+                },
+                "value": "hotfix_pts"
+              }
+            ]
+          },
+          "label": {
+            "type": "plain_text",
+            "text": "Update Type",
+            "emoji": true
+          }
+        }
+      ]
+    }
+  }).catch(err => console.log(err))
 
   // if (req.body.payload.callback_id == "remind") {
   //   console.log(req.body.payload.callback_id == "remind");
   //   return
   // }
 
-  res.send({
-    "title": {
-      "type": "plain_text",
-      "text": "My App",
-      "emoji": true
-    },
-    "submit": {
-      "type": "plain_text",
-      "text": "Submit",
-      "emoji": true
-    },
-    "type": "modal",
-    "close": {
-      "type": "plain_text",
-      "text": "Cancel",
-      "emoji": true
-    },
-    "blocks": [{
-        "type": "section",
-        "text": {
-          "type": "plain_text",
-          "text": "*Contents Update*\n- 점검 시작 30분 전, 10분 전 리마인더\n- 점검 스레드 생성\n- 점검 종료 30분 전, 10분 전 리마인더\n- (Optional) PTS 종료 리마인더\n*Hotfix/PTS*\n- 배포 시작 30분 전, 10분 전, 시작 시점 리마인더",
-          "emoji": true
-        }
-      },
-      {
-        "type": "divider"
-      },
-      {
-        "type": "input",
-        "element": {
-          "type": "static_select",
-          "placeholder": {
-            "type": "plain_text",
-            "text": "Select an item",
-            "emoji": true
-          },
-          "options": [{
-              "text": {
-                "type": "plain_text",
-                "text": "Contents Update",
-                "emoji": true
-              },
-              "value": "cu"
-            },
-            {
-              "text": {
-                "type": "plain_text",
-                "text": "Hotfix/PTS",
-                "emoji": true
-              },
-              "value": "hotfix_pts"
-            }
-          ]
-        },
-        "label": {
-          "type": "plain_text",
-          "text": "Update Type",
-          "emoji": true
-        }
-      }
-    ]
-  });
 });
 
 app.listen(5000, function() {
