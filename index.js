60c764089d6e8d90a029ac5ccaf64cf1567722de
Danny Jang <teru@Dannys-MacBook-Pro-2.local>
2020-08-22 02:19:13 +0900

    Remove slack npm package

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require("body-parser");
-const slack = require("slack");
+//const slack = require("slack");
 const {
   WebClient
 } = require('@slack/web-api'); //official slack web api
