8ee8f5b65350b4723149793d87c64bdb970f6953
Danny Jang <teru@Dannys-MacBook-Pro-2.local>
2020-12-20 04:31:04 +0900

    Fix time bug

--- src/services/reminder_service.js
+++ src/services/reminder_service.js
@@ -87,6 +87,7 @@ function format_reminder(values){
 }
 
 function store_reminder(submission){
+    console.log(submission.target_date);
     const data = {
         update_type: submission.update_type.value,
         start_time: time_service.format_time(submission.target_date, submission.start_time),
--- src/services/time_service.js
+++ src/services/time_service.js
@@ -5,7 +5,5 @@ module.exports = {
 function format_time(date, time){
     var split_date = date.split('-');
     var split_time = time.split(':');
-    var newDate = new Date(split_date[0], split_date[1], split_date[2], split_time[0], split_time[1]);
-    console.log(newDate);
-    return newDate;
+    return new Date(split_date[0], split_date[1], split_date[2], split_time[0], split_time[1]);
 }
