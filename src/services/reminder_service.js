c3daa39673ea8a94b36d22cfe808b823efad873f
Danny Jang <teru@Dannys-MacBook-Pro-2.local>
2020-12-20 01:28:40 +0900

    Fix payload target

--- src/services/reminder_service.js
+++ src/services/reminder_service.js
@@ -42,7 +42,7 @@ function handle_modal(payload){
         }
         else if(type == "block_actions"){
             actions.forEach(action => {
-                console.log("entered foreach loop");
+                console.log("entered foreach loop. action: " + action);
                 if(action.action_id == "update_type"){
                     const update_type = actions.selected_option.value;
                     console.log("entered action_id conditional");
