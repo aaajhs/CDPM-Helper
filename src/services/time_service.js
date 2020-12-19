596f87910e0dcb56b67f6537c902579bd889f95a
Danny Jang <teru@Dannys-MacBook-Pro-2.local>
2020-12-20 04:01:00 +0900

    Fix time bug

--- src/services/time_service.js
+++ src/services/time_service.js
@@ -5,5 +5,5 @@ module.exports = {
 function format_time(date, time){
     var split_date = date.split('-');
     var split_time = time.split(':');
-    return returntime = new Date(split_date[0], split_date[1], split_date[2], split_time[0], split_time[1]);
+    return new Date(split_date[0], split_date[1], split_date[2], split_time[0], split_time[1]);
 }
