72b386bcc28bd22bc94981c0ab0574adc103c7b9
Danny Jang <aaajhs@gmail.com>
2020-12-21 16:51:53 +0900

    Make roulette controller wait for service

--- src/services/roulette_service.js
+++ src/services/roulette_service.js
@@ -26,7 +26,7 @@ async function get_from_db(){
 function post_to_channel(req){
     try{
         const config = get_from_db();
-        console.log("posttochannel: " + config);
+        console.log(config);
         var emoji_pool = config.emoji;
         var last_called = config.last_called;
         var order = config.order;
