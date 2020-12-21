a178f30723fd1a087603e35ceac55163578d6b41
Danny Jang <aaajhs@gmail.com>
2020-12-21 16:39:42 +0900

    Make roulette controller wait for service

--- src/controllers/roulette_controller.js
+++ src/controllers/roulette_controller.js
@@ -6,8 +6,8 @@ module.exports = {
 
 async function get_result(req, res) {
   //let config = await roulette_service.get_from_db();
-  console.log("in controller: " + await roulette_service.get_from_db());
-  roulette_service.post_to_channel(config);
+  // console.log("in controller: " + await roulette_service.get_from_db());
+  roulette_service.post_to_channel(await roulette_service.get_from_db());
 
   res.status(200).send();
 }
